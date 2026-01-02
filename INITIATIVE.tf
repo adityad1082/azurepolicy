26e36a7c053f9d0af045a5c91e20bd3f9f3f8faf
Aditya <aditya.d1547@gmail.com>
2026-01-02 15:20:27 +0530

    Added diagnostic Id in the effect

--- INITIATIVE.tf
+++ INITIATIVE.tf
@@ -25,15 +25,15 @@ resource "azurerm_policy_set_definition" "initiative_policy" {
 
   policy_definition_reference {
     policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/b4fe1a3b-0715-4c6c-a5ea-ffc33cf823cb"
-    parameter_values     = <<VALUE
+
+    parameter_values = <<VALUE
     {
-      "effect": {"value": "DeployIfNotExists"}
-    },
-    {
-      "metricsEnabled": {"value": "/subscriptions/9d07d60f-870c-4033-817b-ba7dfd5a3d71/resourcegroups/terraform-policy-rg/providers/microsoft.operationalinsights/workspaces/dev-workspace"}
+      "effect": {"value": "DeployIfNotExists"},
+      "metricsEnabled": {"value": "/subscriptions/9d07d60f-870c-4033-817b-ba7dfd5a3d71/resourceGroups/terraform-policy-rg/providers/Microsoft.OperationalInsights/workspaces/dev-workspace"}
     }
     VALUE
   }
+
 
   policy_definition_reference {
     policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/c285a320-8830-4665-9cc7-bbd05fc7c5c0"
